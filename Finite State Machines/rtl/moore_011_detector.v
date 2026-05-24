afc5244d3ad28842f96e6edba942798aa50e7b20
RamnarayanBalai <2311401230@stu.manit.ac.in>
2026-05-24 09:09:05 +0530

    feat: add Mealy and Moore 011 detector modules

---
+++ Finite State Machines/rtl/moore_011_detector.v
@@ -0,0 +1,25 @@
+module moore_011_detector(
+    input clk,
+    input reset_n,
+    input x,
+    output y
+);
+
+reg [1:0] state, next_state;
+
+always @(posedge clk, negedge reset_n)
+begin
+    if(!reset_n)
+        state <= 2'b00;
+    else
+        state <= next_state;
+end
+
+always @(*)
+begin
+    next_state[1] = (state[1] ^ state[0]) & x;
+    next_state[0] = (state[1] & ~state[0]) | ~x;
+end
+
+assign y = state[1] & state[0]; // Output depends only on the current state, not on the input
+endmodule
